57ca3e408b8ca0c6deff07af997f0e1bb96d0d04
luckashenri <lucashenrique.23@outlook.com>
2019-12-28 07:19:57 -0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,124 +1,107 @@
-import { Component, Inject, OnInit, OnDestroy, ViewChild } from '@angular/core';
-import { AppService } from './app.service';
-import moment from 'moment';
-import { Moment} from 'moment';
+import { Component, Inject, OnInit, OnDestroy, ViewChild } from "@angular/core";
+import { AppService } from "./app.service";
+import moment from "moment";
+import { Moment } from "moment";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-
-export class AppComponent implements OnInit  {
-  name = 'Angular';
-  data = [];
-
-
-  type: string;
-  dataset = [];
-  options = null;
-  labels: any;
-  legend: boolean;
+export class AppComponent implements OnInit {
+  name = "Angular";
 
   chart;
-
 
   constructor(private appService: AppService) {}
 
   ngOnInit() {
     this.appService.getDataSet().subscribe((res: any[]) => {
-      console.log('original', res)
-      const data = [];
-      const yAxes = [];
-      let opposite = false;
-      res.forEach(variable => {
-        yAxes.push({
-          id: variable.variableName,
-          type: 'linear',
-          position: opposite ? 'right' : 'left'
-        });
-        opposite = !opposite;
-      })
-
-      res.map(variable => {
-        variable.pointsList.forEach(point => {
-          data.push({
-            name: variable.variableName,
-            value: point.value,
-            datetime: point.datetime,
-            unit: variable.variableName
-          })
-        })
-      })
-      this.data = data;
-
-      console.log(this.data)
-
-      let uniqDatetimes;
-      let uniqueNames
-
-      // TO FIND UNIQUE ARRAY
-      var datetimes = data.map(t => t.datetime);
-      uniqDatetimes = datetimes.filter((item, pos) => {
-        return datetimes.indexOf(item) === pos;
-      });
-      console.log('UNIQUE MONTHS:- '+uniqDatetimes);
-
-      var names = data.map(t => t.name);
-      uniqueNames = names.filter((item, pos) => {
-          return names.indexOf(item) === pos;
-      });
-      console.log('UNIQUE NAMES:- '+uniqueNames);
-      var countArr = {};
-      uniqueNames.forEach(d => {
-        var arr = [];
-        uniqDatetimes.forEach(k =>{
-          arr.push(getValue(d, k));
-        });
-        countArr[d] = arr;
-        this.dataset.push({
-          id: d,
-          label: d,
-          data: arr,
-          yAxisID: d
-        })
-      });
-      console.log('COUNT ARRAY:- '+JSON.stringify(countArr));
-
-      // To get value from the array
-      function getValue(name, datetime){
-        var value = 0;
-        data.forEach(d => {
-          if (d.name === name && d.datetime === datetime){
-            value = d.value;
-          }
-        });
-        return value;
-      }
-
-
-      console.log('dataset', this.dataset)
-
-      this.labels = uniqueNames;
-      this.options = {
-        scales: {yAxes: yAxes},
-      };
-      this.legend = true;
-      this.type = 'bar';
-
-      this.chart = {
-        options: this.options,
-        dataset: this.dataset,
-        legend: true,
-        labels: uniqDatetimes,
-        chartType: 'bar'
-      }
-
-      console.log('chart', this.chart)
-    })
-
-
-
+      this.chart = this.mountGraph(res);
+    });
   }
 
+  mountGraph(payload) {
+    console.log("original", payload);
+    const data = [];
+    const yAxes = [];
+    let dataset = [];
+    let chart;
+    let opposite = false;
+
+    payload.forEach(variable => {
+      yAxes.push({
+        id: variable.variableName,
+        type: "linear",
+        position: opposite ? "right" : "left"
+      });
+      opposite = !opposite;
+    });
+
+    payload.map(variable => {
+      variable.pointsList.forEach(point => {
+        data.push({
+          name: variable.variableName,
+          value: point.value,
+          datetime: point.datetime,
+          unit: variable.variableName
+        });
+      });
+    });
+
+    let uniqDatetimes;
+    let uniqueNames;
+
+    // TO FIND UNIQUE ARRAY
+    var datetimes = data.map(t => t.datetime);
+    uniqDatetimes = datetimes.filter((item, pos) => {
+      return datetimes.indexOf(item) === pos;
+    });
+    console.log("UNIQUE MONTHS:- " + uniqDatetimes);
+
+    var names = data.map(t => t.name);
+    uniqueNames = names.filter((item, pos) => {
+      return names.indexOf(item) === pos;
+    });
+    console.log("UNIQUE NAMES:- " + uniqueNames);
+    var countArr = {};
+    uniqueNames.forEach(d => {
+      var arr = [];
+      uniqDatetimes.forEach(k => {
+        arr.push(getValue(d, k));
+      });
+      countArr[d] = arr;
+      dataset.push({
+        id: d,
+        label: d,
+        data: arr,
+        yAxisID: d
+      });
+    });
+    console.log("COUNT ARRAY:- " + JSON.stringify(countArr));
+
+    // To get value from the array
+    function getValue(name, datetime) {
+      var value = 0;
+      data.forEach(d => {
+        if (d.name === name && d.datetime === datetime) {
+          value = d.value;
+        }
+      });
+      return value;
+    }
+
+    console.log("dataset", dataset);
+
+    chart = {
+      options: { scales: { yAxes: yAxes } },
+      dataset: dataset,
+      legend: true,
+      labels: uniqDatetimes,
+      chartType: "bar"
+    };
+
+    console.log("chart", chart);
+    return chart;
+  }
 }
